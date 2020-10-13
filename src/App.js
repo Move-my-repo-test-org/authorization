c060f601415f5ac88b759be1fbd85e5e2ccddef7
NastiaKoval <anasteziyam@gmail.com>
2020-10-14 00:06:41 +0300

    feat: add create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AuthorizationPage from './components/AuthorizationPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthorizationPage />
+    </div>
+  );
+}
+
+export default App;
